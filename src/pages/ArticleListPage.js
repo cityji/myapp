ed525f908edb405f131a5cb8bab74adb4bf51151
cityji <edu.nikhiltiwari@gmail.com>
2023-07-15 12:43:56 +0530

    encapsulates article list by using probs and component templates

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,15 +1,11 @@
 import articles from './article-content';
-import {Link} from 'react-router-dom';
+import ArticleList from '../components/ArticlesList';
 const ArticleListPage=()=>{
     return(
         <>
-        <h1>Articles List </h1>
-        {articles.map((article,key)=>(
-           <Link className='article-list-item' to={`/articles/${article.name}`} key={key}>
-            <h3 style={{textAlign:'left'}} key={key}>{article.title}</h3>
-            <p className='article-para'>{article.content[0].substring(0,150)}...</p>
-           </Link>
-        ))}
+        <h1>Articles  </h1>
+
+        <ArticleList articles={articles} />
         </>
     )
 }
