03e2d42be947a03b4dca98c6f27325949d7e5990
cityji <edu.nikhiltiwari@gmail.com>
2023-07-15 18:45:34 +0530

    FrontEnd Complete!!

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,12 @@
 // how to get get user get parameter localhost://3000/learn-react we're trying to get "learn-react" string
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 const ArticlePage=()=>{
     const {articleId} = useParams();
     const article = articles.find(article=>article.name===articleId);
+    if(!article) return <NotFoundPage />
+    
     return(
         <>
         <h1>{article.title}</h1>
